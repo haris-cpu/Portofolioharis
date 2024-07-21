401fa5919dc83f1522188a6326942367f6b45771
haris-cpu <harisjabbar24@gmail.com>
2024-07-22 00:25:13 +0500

    Add files via upload
    ok

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".text", {
+        strings: ["Front-End Developer", "Digital Marketing Expert", "Extrovert"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
